a1ead4cc2a54a1421482514c062faff2b6954432
Omnilon <islano.co@gmail.com>
2025-11-05 09:39:01 -0500

    Legger3

---
+++ site/.next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"components/GradientBG.tsx -> three\":{\"id\":\"components/GradientBG.tsx -> three\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_three_build_three_module_js.js\"]}}"
---
+++ site/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/omnilon/Desktop/OmniLend/site/src/app/layout\":[\"static/media/bb3ef058b751a6ad-s.p.woff2\",\"static/media/36966cca54120369-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
